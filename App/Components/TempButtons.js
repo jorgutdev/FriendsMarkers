fc24c7568eefbc25c89bca369743b0c6b6326b3b
Jorge Gutierrez <jorge@Jorges-MacBook-Pro.local>
2017-09-16 21:09:21 +0200

    New modal

--- App/Components/TempButtons.js
+++ App/Components/TempButtons.js
@@ -1,82 +1,95 @@
-import React, { Component } from 'react'
-import { TouchableOpacity, Image, View, Text, StyleSheet } from 'react-native'
-import { connect } from 'react-redux'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import ActionButton from 'react-native-action-button';
-
-
-export class TempButtons extends Component {
-
-    
-
-
-
-    constructor(props) {
-        super(props)
-    }
-
-    componentDidMount() {
-    }
-
-
-
-    render() {
-
-        return (
-            <View style={{ flex: 1 }}>
-               <View>
-
-
-
-                <ActionButton 
-            buttonColor="rgba(0,0,0,1)"
-             onPress={ () => {
-                alert('cancel')
-                }}  icon={
-                    
-                    <Icon name="cancel" />
-      
-                }/>
-              </View>
-            <ActionButton buttonColor="rgba(250,0,0,1)" onPress={ () => {
-                alert('save')}}
-                icon={
-                    
-                    <Icon name="cancel"  />
-      
-                }
-                 />
-
-
-            </View>
-        )
-    }
-}
-
+import React, { Component } from "react";
+import {
+  TouchableOpacity,
+  Image,
+  View,
+  Text,
+  StyleSheet,
+  ToastAndroid
+} from "react-native";
+import { connect } from "react-redux";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import ActionButton from "react-native-action-button";
 
 var styles = StyleSheet.create({
-    centerButton: {
-        alignSelf: 'flex-start',
-    },
-    buttonContainerLeft: {
-        alignSelf: 'flex-start'
-    },
-    buttonCancel: {
-        color:'white'
-    }
-})
+  container: {
+    flex: 1,
+    alignSelf: "center",
+    flexDirection: "row",
+    width: "100%",
+    bottom: "-100%"
+  },
+  cancelButton: {
+    flex: 1,
+    height: "20%",
+    left: "4%"
+  },
+  cancelIcon: {
+    color: "white",
+    fontSize: 20
+  },
+  okButton: {
+    flex: 1,
+    height: "20%",
+    left: "-17%"
+  },
+  okIcon: {
+    color: "black",
+    fontSize: 20
+  }
+});
+
+export class TempButtons extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    ToastAndroid.showWithGravity(
+      "Adding marker\nPress the marker to move it",
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.cancelButton}>
+          <ActionButton
+            buttonColor="rgba(20, 4, 4,1.0)"
+            onPress={this.props.onCancel}
+            icon={
+              <Icon
+                name="close"
+                style={styles.cancelIcon}
+              />
+            }
+          />
+        </View>
+        <View style={styles.okButton}>
+          <ActionButton
+            buttonColor="rgba(82, 255, 30,1)"
+            onPress={this.props.onSave}            
+            icon={
+              <Icon
+                name="check"
+                style={styles.okIcon}
+              />
+            }
+          />
+        </View>
+      </View>
+    );
+  }
+}
 
 function mapStateToProps(state) {
-    return {
-    }
+  return {};
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-    }
+  return {};
 }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(TempButtons)
+export default connect(mapStateToProps, mapDispatchToProps)(TempButtons);
