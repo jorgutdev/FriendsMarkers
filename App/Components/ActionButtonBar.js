9379d0408759d3793cc84c2fb6b19aab24302cd6
Jorge <jorgutdev@gmail.com>
2017-10-20 10:26:52 +0200

    Prettier commit

--- App/Components/ActionButtonBar.js
+++ App/Components/ActionButtonBar.js
@@ -1,70 +1,58 @@
-import React, { Component } from 'react'
-import { TouchableOpacity, Image, View, Text, StyleSheet } from 'react-native'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { connect } from 'react-redux'
-import ActionButton from 'react-native-action-button';
-
-
+import React, { Component } from "react";
+import { TouchableOpacity, Image, View, Text, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { connect } from "react-redux";
+import ActionButton from "react-native-action-button";
 
 export class ActionButtonBar extends Component {
+  showModal = () => {
+    this.props.showModal();
+  };
 
-
-
-
-    constructor(props) {
-        super(props)
-    }
-
-    componentDidMount() {
-    }
-
-
-    showModal = () => {
-        this.props.showModal()
-    }
-
-    render() {
-
-        return (
-            <ActionButton buttonColor="rgba(231,76,60,1)" position='left' verticalOrientation='down' style={styles.buttonContainer}>
-                <ActionButton.Item buttonColor='#9b59b6' onPress={this.showModal}>
-                    <Icon name="map-marker-plus" style={styles.actionButtonIcon} />
-                </ActionButton.Item>
-                <ActionButton.Item buttonColor='#3498db' onPress={() => this.props.navigation.navigate('DrawerOpen')}>
-                    <Icon name="account" style={styles.actionButtonIcon} />
-                </ActionButton.Item>
-                <ActionButton.Item buttonColor='#1abc9c' onPress={() => { }}>
-                    <Icon name="map-marker" style={styles.actionButtonIcon} />
-                </ActionButton.Item>
-            </ActionButton>
-        )
-    }
+  render() {
+    return (
+      <ActionButton
+        buttonColor="rgba(231,76,60,1)"
+        position="left"
+        verticalOrientation="down"
+        style={styles.buttonContainer}
+      >
+        <ActionButton.Item buttonColor="#9b59b6" onPress={this.showModal}>
+          <Icon name="map-marker-plus" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item
+          buttonColor="#3498db"
+          onPress={() => this.props.navigation.navigate("DrawerOpen")}
+        >
+          <Icon name="account" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item buttonColor="#1abc9c" onPress={() => {}}>
+          <Icon name="map-marker" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+      </ActionButton>
+    );
+  }
 }
 
-
 var styles = StyleSheet.create({
-    actionButtonIcon: {
-        color: '#fff',
-        fontSize: 18,
-    },
-    buttonContainer: {
-        top: '9%',
-        left: '-2%'
-    }
-})
+  actionButtonIcon: {
+    color: "#fff",
+    fontSize: 18
+  },
+  buttonContainer: {
+    top: "9%",
+    left: "-2%"
+  }
+});
 
 function mapStateToProps(state) {
-    return {
-        isLogged: state.userReducer.isLogged,
-    }
+  return {
+    isLogged: state.userReducer.isLogged
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-    }
+  return {};
 }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(ActionButtonBar)
+export default connect(mapStateToProps, mapDispatchToProps)(ActionButtonBar);
--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,17 @@
-import './App/Config/ReactotronConfig'
-import { AppRegistry } from 'react-native'
-import App from './App/Containers/App'
+import "./App/Config/ReactotronConfig";
+import { AppRegistry } from "react-native";
+import App from "./App/Containers/App";
+import * as firebase from "firebase";
 
-AppRegistry.registerComponent('FriendsMarkers', () => App)
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCNeVXNwwn21u2Rfoevif3-R0zor4qkl90",
+  authDomain: "friendsmarkers-7fb19.firebaseapp.com",
+  databaseURL: "https://friendsmarkers-7fb19.firebaseio.com",
+  projectId: "friendsmarkers-7fb19",
+  storageBucket: "friendsmarkers-7fb19.appspot.com",
+  messagingSenderId: "114090560885"
+};
+
+firebase.initializeApp(firebaseConfig);
+AppRegistry.registerComponent("FriendsMarkers", () => App);
